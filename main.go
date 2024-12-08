523789f573536b3e4f5d67e19a06a55003f66b6f
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-12-08 10:49:08 +0100

    wtf

--- main.go
+++ main.go
@@ -1,77 +1,99 @@
 package main
 
 import (
-	"flag"
-	"io"
-	"log"
+	"fmt"
+	"github.com/google/go-cmp/cmp"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/restmapper"
 	"os"
+	"strings"
+	"time"
 
-	"github.com/ibuildthecloud/wtfk8s/pkg/differ"
-	"github.com/ibuildthecloud/wtfk8s/pkg/watcher"
-	"github.com/rancher/wrangler/pkg/clients"
-	"github.com/rancher/wrangler/pkg/generic"
-	"github.com/rancher/wrangler/pkg/kubeconfig"
-	"github.com/rancher/wrangler/pkg/signals"
+	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+
 	"github.com/sirupsen/logrus"
-	"k8s.io/klog/v2"
 )
 
 var (
-	kubeConfig = flag.String("kubeconfig", "", "Kube config location")
-	context    = flag.String("context", "", "Kube config context")
-	namespace  = flag.String("namespace", "", "Limit to namespace")
+	kubeConfig string
+	namespace  string
 )
 
-func main() {
-	flag.Parse()
-	if err := mainErr(); err != nil {
-		log.Fatalln(err)
+func run(cmd *cobra.Command, args []string) error {
+	cfg, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		return err
 	}
+
+	cli, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	disc := discovery.NewDiscoveryClientForConfigOrDie(cfg)
+	groupResources, err := restmapper.GetAPIGroupResources(disc)
+	if err != nil {
+		return err
+	}
+
+	gvr := schema.GroupVersionResource{}
+
+	switch strings.Count(args[0], ".") {
+	case 0:
+		gvr.Resource = args[0]
+	case 1:
+		s := strings.SplitN(args[0], ".", 2)
+		gvr.Resource = s[0]
+		gvr.Group = s[1]
+	default:
+		return fmt.Errorf("unknown group version %q", args[0])
+	}
+
+	for _, groupResource := range groupResources {
+		//
+	}
+
+	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(cli, time.Minute, namespace, nil)
+	informer := factory.ForResource(gvr).Informer()
+
+	_, err = informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		UpdateFunc: func(oldObj interface{}, newObj interface{}) {
+			fmt.Printf("%s\n", cmp.Diff(oldObj, newObj))
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	<-cmd.Context().Done()
+
+	return nil
+
 }
 
-func mainErr() error {
-	klog.SetOutput(io.Discard)
-	logrus.SetLevel(logrus.FatalLevel)
+func main() {
+	// klog.SetOutput(io.)
+	logrus.SetLevel(logrus.InfoLevel)
 
-	ctx := signals.SetupSignalContext()
-	restConfig := kubeconfig.GetNonInteractiveClientConfigWithContext(*kubeConfig, *context)
-
-	clients, err := clients.New(restConfig, &generic.FactoryOptions{
-		Namespace: *namespace,
-	})
-	if err != nil {
-		return err
+	cmd := &cobra.Command{
+		Use:          "wtfk8s",
+		Short:        "wtfk8s",
+		SilenceUsage: true,
+		RunE:         run,
 	}
 
-	watcher, err := watcher.New(clients)
-	if err != nil {
-		return err
+	cmd.Flags().StringVar(&kubeConfig, "kubeconfig", os.Getenv("KUBECONFIG"), "")
+	cmd.Flags().StringVarP(&namespace, "namespace", "n", corev1.NamespaceAll, "")
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	for _, arg := range os.Args[1:] {
-		watcher.MatchName(arg)
-	}
-
-	differ, err := differ.New(clients)
-	if err != nil {
-		return err
-	}
-
-	objs, err := watcher.Start(ctx)
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		for obj := range objs {
-			_ = differ.Print(obj)
-		}
-	}()
-
-	if err := clients.Start(ctx); err != nil {
-		return err
-	}
-
-	<-ctx.Done()
-	return nil
 }
